0a2e090e4439ba47d3ded002fce4188aeeb9ee3e
dzozrawr <jovan_96@live.ru>
2023-02-23 17:56:26 +0100

    added bezier path

--- Assets/_Game/Scripts/Plate.cs
+++ Assets/_Game/Scripts/Plate.cs
@@ -4,18 +4,19 @@ using PlayerNamespace;
 using UnityEngine;
 using HoldableNameSpace;
 using Ingredients;
+using DG.Tweening;
 
 public class Plate : HoldableObject
 {
-    public List<Transform> placesForIngredients=null;
-    public Transform transformValuesWhenHeld=null;
+    public List<Transform> placesForIngredients = null;
+    public Transform transformValuesWhenHeld = null;
     public PlateScriptableObject plateScriptableObject = null;
     private PlayerController playerController = null;
 
-    private List<PreparedIngredient> ingredients=new List<PreparedIngredient>();
+    private List<PreparedIngredient> ingredients = new List<PreparedIngredient>();
     private int ingrPlaceInd = 0;
 
-    private GameObject newPlate=null;
+    private GameObject newPlate = null;
 
     public List<PreparedIngredient> Ingredients { get => ingredients; set => ingredients = value; }
 
@@ -24,11 +25,13 @@ public class Plate : HoldableObject
         if (other.gameObject.tag.Equals("Player"))
         { //if player collides
             playerController = other.gameObject.GetComponent<PlayerController>();
-            if(ingrPlaceInd<placesForIngredients.Count)
+            if (ingrPlaceInd < placesForIngredients.Count)
             {   //if plate not full would be a better question for this if statement
-                if(playerController.PlayerState==PlayerStates.Holding){ //maybe this is a redundant state
-                    if(playerController.HeldObject.type==HoldableType.PreparedIngred){
-                        HoldableObject preparedIngred=playerController.HeldObject;
+                if (playerController.PlayerState == PlayerStates.Holding)
+                { //maybe this is a redundant state
+                    if (playerController.HeldObject.type == HoldableType.PreparedIngred)
+                    {
+                        HoldableObject preparedIngred = playerController.HeldObject;
                         ingredients.Add((PreparedIngredient)playerController.HeldObject);
 
                         preparedIngred.transform.position = placesForIngredients[ingrPlaceInd].position;
@@ -39,27 +42,36 @@ public class Plate : HoldableObject
                         ingrPlaceInd++;
                     }
                 }
-            }else{
-                if(playerController.PlayerState==PlayerStates.Default){
+            }
+            else
+            {
+                if (playerController.PlayerState == PlayerStates.Default)
+                {
                     //Quaternion oldRot= transform.rotation;
-
-                    Instantiate(plateScriptableObject.platePrefab, transform.position,Quaternion.identity).transform.SetParent(transform.parent); 
+                    newPlate=Instantiate(plateScriptableObject.platePrefab, transform.position, Quaternion.identity);
+                    newPlate.transform.SetParent(transform.parent);
+                    newPlate.SetActive(false);
+                    
+                    Invoke(nameof(ShowNewPlateAfterDelay),0.5f);
                     playerController.SetHoldableObject(this);
 
-                    transform.localPosition=transformValuesWhenHeld.localPosition;
-                    transform.localRotation=transformValuesWhenHeld.localRotation;
+                    transform.localPosition = transformValuesWhenHeld.localPosition;
+                    transform.localRotation = transformValuesWhenHeld.localRotation;
 
                     //transform.up=Vector3.up;
-                    GetComponent<Collider>().enabled=false;
+                    GetComponent<Collider>().enabled = false;
                 }
             }
         }
     }
-    
 
-    private void RotateAfterDelay(){
-        //transform.Tra
-        //transform.localRotation=Quaternion.Euler(0,0,0);
-     //   transform.localRotation=Quaternion.Euler(-17.7f,26.9f,29.368f);
+
+
+    private void ShowNewPlateAfterDelay()
+    {
+        Vector3 plateDefaultScale=newPlate.transform.localScale;
+        newPlate.transform.localScale=Vector3.zero;
+        newPlate.SetActive(true);
+        newPlate.transform.DOScale(plateDefaultScale,0.33f);
     }
 }
