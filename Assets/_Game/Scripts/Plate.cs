7ad2582f48868c93c662dc92a2d2d921ab9f617b
dzozrawr <jovan_96@live.ru>
2023-02-27 13:23:35 +0100

    made the system for combining ingredients into a model on a plate, needs beautifying

--- Assets/_Game/Scripts/Plate.cs
+++ Assets/_Game/Scripts/Plate.cs
@@ -17,8 +17,11 @@ public class Plate : HoldableObject
     }
 
     public List<Transform> placesForIngredients = null;
+    public Transform placeForFoodModel = null;
     public Transform transformValuesWhenHeld = null;
     public PlateScriptableObject plateScriptableObject = null;
+    public List<PlateFood> plateFoods = null;
+
     private PlayerController playerController = null;
 
     private List<PreparedIngredient> ingredients = new List<PreparedIngredient>();
@@ -33,9 +36,9 @@ public class Plate : HoldableObject
         if (other.gameObject.tag.Equals("Player"))
         { //if player collides
             playerController = other.gameObject.GetComponent<PlayerController>();
-            if (ingrPlaceInd < placesForIngredients.Count)
+            if (playerController.PlayerState == PlayerStates.Holding)
             {   //if plate not full would be a better question for this if statement
-                if (playerController.PlayerState == PlayerStates.Holding)
+                if (ingrPlaceInd < placesForIngredients.Count)
                 { //maybe this is a redundant state
                     if (playerController.HeldObject.type == HoldableType.PreparedIngred)
                     {
@@ -56,11 +59,16 @@ public class Plate : HoldableObject
                 if (playerController.PlayerState == PlayerStates.Default)
                 {
                     //Quaternion oldRot= transform.rotation;
-                    newPlate=Instantiate(plateScriptableObject.platePrefab, transform.position, Quaternion.identity);
+                    newPlate = Instantiate(plateScriptableObject.platePrefab, transform.position, Quaternion.identity);
                     newPlate.transform.SetParent(transform.parent);
                     newPlate.SetActive(false);
-                    
-                    Invoke(nameof(ShowNewPlateAfterDelay),0.5f);
+
+                    Invoke(nameof(ShowNewPlateAfterDelay), 0.5f);
+
+                    GameObject foodModel = FindPlateFood(ingredients);
+                    if (foodModel != null) Debug.Log("Food model found!"); else Debug.Log("Food model NOT found!");
+                    SetPlateFoodModel(foodModel);
+
                     playerController.SetHoldableObject(this);
 
                     transform.localPosition = transformValuesWhenHeld.localPosition;
@@ -73,13 +81,65 @@ public class Plate : HoldableObject
         }
     }
 
+    private void SetPlateFoodModel(GameObject foodModel)
+    {
+        if (foodModel == null) return;
+        for (int i = 0; i < placesForIngredients.Count; i++)
+        {
+            if (placesForIngredients[i].childCount > 0)
+            {
+                Destroy(placesForIngredients[i].GetChild(0).gameObject);
+                //placesForIngredients[i].GetChild(0).gameObject;
+            }
+        }
+        foodModel.transform.position = placeForFoodModel.position;
+        foodModel.transform.SetParent(placeForFoodModel);
+    }
 
+    private GameObject FindPlateFood(List<PreparedIngredient> prepIngrList)
+    {
+        IngredientType[] ingredientTypes = new IngredientType[prepIngrList.Count];
+        for (int i = 0; i < ingredientTypes.Length; i++)
+        {
+            ingredientTypes[i] = prepIngrList[i].ingrType;
+        }
+        return FindPlateFood(ingredientTypes);
+    }
 
+    private GameObject FindPlateFood(IngredientType[] ingrList)
+    {
+        bool isMatching = false;
+        for (int i = 0; i < plateFoods.Count; i++)
+        {
+            if (plateFoods[i].ingredients.Count == ingrList.Length)
+            {
+                for (int j = 0; j < plateFoods[i].ingredients.Count; j++)
+                {
+                    for (int k = 0; k < ingrList.Length; k++)
+                    {
+                        if (plateFoods[i].ingredients[j] == ingrList[k])
+                        {
+                            isMatching = true;
+                            break;
+                        }
+                        if ((k + 1) == ingrList.Length) isMatching = false;
+                    }
+                    if (!isMatching) break;
+
+                    if ((j + 1) == plateFoods[i].ingredients.Count) //here all the ingredients match
+                    {
+                        return Instantiate(plateFoods[i].foodPrefab);
+                    }
+                }
+            }
+        }
+        return null;
+    }
     private void ShowNewPlateAfterDelay()
     {
-        Vector3 plateDefaultScale=newPlate.transform.localScale;
-        newPlate.transform.localScale=Vector3.zero;
+        Vector3 plateDefaultScale = newPlate.transform.localScale;
+        newPlate.transform.localScale = Vector3.zero;
         newPlate.SetActive(true);
-        newPlate.transform.DOScale(plateDefaultScale,0.33f);
+        newPlate.transform.DOScale(plateDefaultScale, 0.33f);
     }
 }
