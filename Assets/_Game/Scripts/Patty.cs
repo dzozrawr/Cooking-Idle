d4b9b2623f738fdc6077ce5d42a74d9a20c19175
dzozrawr <jovan_96@live.ru>
2023-03-02 12:59:08 +0100

    changed patty animation to have a delay at the beginning

--- Assets/_Game/Scripts/Patty.cs
+++ Assets/_Game/Scripts/Patty.cs
@@ -9,9 +9,14 @@ public class Patty : PanFryableIngredient
     protected override void InitTween()
     {
         jumpSequence = DOTween.Sequence();
+
+        jumpSequence.AppendInterval(0.33f);
+
         jumpSequence.Append(ingredientParentTransform.DOLocalMove(jumpHighPos.position, 0.33f));
         jumpSequence.Append(ingredientParentTransform.DOLocalMove(ingredientDefaultPos, 0.33f));
-        jumpSequence.AppendInterval(0.33f);
+
+        //jumpSequence.AppendInterval(0.165f);
+
         jumpSequence.SetLoops(-1);
         jumpSequence.Pause();
 
@@ -19,12 +24,15 @@ public class Patty : PanFryableIngredient
         // rotateSequence.Append(ingredientTransform.DOBlendableLocalRotateBy(new Vector3(0, 0, 270), 0.33f));
         // rotateSequence.Append(ingredientTransform.DOBlendableLocalRotateBy(new Vector3(0, 0, 270), 0.33f));
 
+        rotateSequence.AppendInterval(0.33f);
+
         rotateSequence.Append(ingredientParentTransform.DOBlendableRotateBy(new Vector3(0, 0, 180), 0.22f));
         rotateSequence.Append(ingredientParentTransform.DOBlendableRotateBy(new Vector3(0, 0, 90), 0.11f));
         rotateSequence.Append(ingredientParentTransform.DOBlendableRotateBy(new Vector3(0, 0, 180), 0.22f));
         rotateSequence.Append(ingredientParentTransform.DOBlendableRotateBy(new Vector3(0, 0, 90), 0.11f));
 
-        rotateSequence.AppendInterval(0.33f);
+       // rotateSequence.AppendInterval(0.165f);
+
         rotateSequence.SetLoops(-1);
         rotateSequence.Pause();
     }
